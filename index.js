6dfa354c227ac304146d5d5101af722300c98002
Daniel <danbapp@gmail.com>
2021-10-24 13:53:09 -0400

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+
+function findMatching(array, value){
+  const selected = array.filter(array => array.toUpperCase() === value.toUpperCase());
+  return selected;
+}
+
+function fuzzyMatch(array, value){
+  const selected = array.filter(array => array.substring(0, 2) === value.substring(0, 2));
+  return selected;
+}
+
+function matchName(array, value){
+  const selected = array.filter(array => array.name === value);
+  return selected;
+}
